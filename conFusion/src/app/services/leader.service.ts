5be3e4e499ea21fa753abca371d641fae8c23a0c
Bikash Kumar Sahoo <biki.sahoo123@gmail.com>
2021-10-04 15:20:43 +0530

    w3 rxjs part 1 pbservables

--- conFusion/src/app/services/leader.service.ts
+++ conFusion/src/app/services/leader.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Observable ,of } from 'rxjs';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
-
+import { delay } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,25 +10,31 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Promise<Leader[]> {
+  getLeaders(): Observable<Leader[]> {
     // return Promise.resolve(LEADERS);
-    return new Promise((resolve)=>{
-      setTimeout(()=> resolve(LEADERS),2000);
-    });
+    // return new Promise((resolve)=>{
+    //   setTimeout(()=> resolve(LEADERS),2000);
+    // });
+
+    return of(LEADERS).pipe(delay(2000));
   }
 
-  getLeader(id:string): Promise<Leader> {
+  getLeader(id:string): Observable<Leader> {
     // return Promise.resolve(LEADERS.filter((leader) => ( leader.id===id) )[0]);
-    return new Promise( (resolve) =>{
-      setTimeout(() => resolve(LEADERS.filter((leader) => ( leader.id===id) )[0]),2000);
-    })
+    // return new Promise( (resolve) =>{
+    //   setTimeout(() => resolve(LEADERS.filter((leader) => ( leader.id===id) )[0]),2000);
+    // })
+    return of(LEADERS.filter((leader) => ( leader.id===id) )[0]).pipe(delay(2000));
   }
 
-  getFeaturedLeader(): Promise<Leader> {
+  getFeaturedLeader(): Observable<Leader> {
     // return Promise.resolve(LEADERS.filter( (leader) => leader.featured)[0]);
-    return new Promise( (resolve) =>{
-      setTimeout(() => resolve(LEADERS.filter( (leader) => leader.featured)[0]),2000);
-    })
+    // return new Promise( (resolve) =>{
+    //   setTimeout(() => resolve(LEADERS.filter( (leader) => leader.featured)[0]),2000);
+    // })
+
+    return of(LEADERS.filter( (leader) => leader.featured)[0]).pipe(delay(2000));
+
   }
 
 }
--- conFusion/src/app/services/promotion.service.ts
+++ conFusion/src/app/services/promotion.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Observable ,of } from 'rxjs';
 import { Promotion } from '../shared/promotion';
 import { PROMOTION } from '../shared/promotions';
+import { delay } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,24 +11,32 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotiones(): Promise<Promotion[]> {
+  getPromotiones(): Observable<Promotion[]> {
     // return Promise.resolve(PROMOTION);
-    return new Promise((resolve) => {
-      setTimeout(()=>resolve(PROMOTION),2000);
-    })
+    // return new Promise((resolve) => {
+    //   setTimeout(()=>resolve(PROMOTION),2000);
+    // })
+
+    return of(PROMOTION).pipe(delay(2000));
   }
 
-  getPromotion(id:string): Promise<Promotion> {
+  getPromotion(id:string): Observable<Promotion> {
     // return Promise.resolve(PROMOTION.filter((promotion) => ( promotion.id===id) )[0]);
-    return new Promise((resolve) => {
-      setTimeout(()=>resolve(PROMOTION.filter((promotion) => ( promotion.id===id) )[0]),2000);
-    })
+    // return new Promise((resolve) => {
+    //   setTimeout(()=>resolve(PROMOTION.filter((promotion) => ( promotion.id===id) )[0]),2000);
+    // })
+
+    return of(PROMOTION.filter((promotion) => ( promotion.id===id) )[0]).pipe(delay(2000));
+
   }
 
-  getFeaturedPromotion(): Promise<Promotion> {
+  getFeaturedPromotion(): Observable<Promotion> {
     // return Promise.resolve(PROMOTION.filter( (promotion) => promotion.featured)[0]);
-    return new Promise((resolve) => {
-      setTimeout(()=>resolve(PROMOTION.filter( (promotion) => promotion.featured)[0]),2000);
-    })
+    // return new Promise((resolve) => {
+    //   setTimeout(()=>resolve(PROMOTION.filter( (promotion) => promotion.featured)[0]),2000);
+    // })
+
+    return of(PROMOTION.filter( (promotion) => promotion.featured)[0]).pipe(delay(2000));
+
   }
 }
