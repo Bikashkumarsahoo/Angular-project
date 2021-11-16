ca1de26b5b3ab836f3c02f82ad6042f97640bc20
Bikash Kumar Sahoo <biki.sahoo123@gmail.com>
2021-11-16 16:45:56 +0530

    W4.2. saving changes to server

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, ViewChild, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { Params,ActivatedRoute } from '@angular/router';
+import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
@@ -10,71 +10,94 @@ import { Comment } from '../shared/comment';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
 })
 export class DishdetailComponent implements OnInit {
-
-  
   dish!: Dish;
+  dishCopy!: Dish;
   errMess!: string;
-  dishIds!:string [];
+  dishIds!: string[];
   prev!: string;
   next!: string;
   commentForm!: FormGroup;
-  comment!:Comment;
+  comment!: Comment;
   @ViewChild('cform') commentFormDirective: any;
 
-  commentFormErrors:any={
-    'author': '',
-    'comment': '',
-    'rating':''
+  commentFormErrors: any = {
+    author: '',
+    comment: '',
+    rating: '',
   };
 
-  validationMessages:any = {
-    'comment': {
-      'required':      'Comment is required.',
+  validationMessages: any = {
+    comment: {
+      required: 'Comment is required.',
     },
-    'author': {
-      'required':      'Author Name is required.',
-      'minlength':     'Author Name must be at least 2 characters long.',
-      'maxlength':     'Author Name cannot be more than 35 characters long.'
+    author: {
+      required: 'Author Name is required.',
+      minlength: 'Author Name must be at least 2 characters long.',
+      maxlength: 'Author Name cannot be more than 35 characters long.',
     },
   };
 
-  constructor(private dishService:DishService,private route:ActivatedRoute, private location:Location, private formBuilder:FormBuilder, @Inject('BaseURL') public BaseURL:any ) { 
+  constructor(
+    private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private formBuilder: FormBuilder,
+    @Inject('BaseURL') public BaseURL: any
+  ) {
     this.createForm();
   }
 
   ngOnInit(): void {
-    this.dishService.getDishIds()
-      .subscribe((dishIds) => { this.dishIds=dishIds});
+    this.dishService.getDishIds().subscribe((dishIds) => {
+      this.dishIds = dishIds;
+    });
     this.route.params
-      .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-      .subscribe( (dish) => {this.dish= dish; this.setPrevNext(dish.id); },
-      errmess => this.errMess=<any>errmess);
+      .pipe(
+        switchMap((params: Params) => this.dishService.getDish(params['id']))
+      )
+      .subscribe(
+        (dish) => {
+          this.dish = dish;
+          this.dishCopy = dish;
+          this.setPrevNext(dish.id);
+        },
+        (errmess) => (this.errMess = <any>errmess)
+      );
   }
 
-  createForm(){
-    this.commentForm=this.formBuilder.group({
-      author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(35)]],
+  createForm() {
+    this.commentForm = this.formBuilder.group({
+      author: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(35),
+        ],
+      ],
       comment: ['', Validators.required],
-      rating:[5, Validators.required]
+      rating: [5, Validators.required],
     });
-    this.commentForm.valueChanges.subscribe(data => this.onValueChange(data));
+    this.commentForm.valueChanges.subscribe((data) => this.onValueChange(data));
     this.onValueChange();
   }
-  onValueChange(data?: any){
-    if(!this.commentForm) { return; }
-    const form=this.commentForm;
-    for(const field in this.commentFormErrors) {
-      if(this.commentFormErrors.hasOwnProperty(field)) {
-        this.commentFormErrors[field]='';
-        const control=form.get(field);
-        if(control && control.dirty && !control.valid) {
-          const messages=this.validationMessages[field];
-          for( const key in control.errors){
-            if(control.errors.hasOwnProperty(key)){
-              this.commentFormErrors[field]+=messages[key];
+  onValueChange(data?: any) {
+    if (!this.commentForm) {
+      return;
+    }
+    const form = this.commentForm;
+    for (const field in this.commentFormErrors) {
+      if (this.commentFormErrors.hasOwnProperty(field)) {
+        this.commentFormErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.commentFormErrors[field] += messages[key];
             }
           }
         }
@@ -82,28 +105,40 @@ export class DishdetailComponent implements OnInit {
     }
   }
 
-  onSubmit(){
+  onSubmit() {
     let date = new Date().toISOString();
     this.comment = this.commentForm.value;
     this.comment.date = date;
-    this.dish.comments.push(this.comment);
+    this.dishCopy.comments.push(this.comment);
+    this.dishService.putDish(this.dishCopy).subscribe(
+      (dish) => {
+        this.dish = dish;
+        this.dishCopy = dish;
+      },
+      (errmess) => {
+        this.dish = null as any;
+        this.dishCopy = null as any;
+        this.errMess = <any>errmess;
+      }
+    );
     console.log(this.comment);
     this.commentForm.reset({
       author: '',
       comment: '',
-      rating:5
+      rating: 5,
     });
     this.commentFormDirective.resetForm();
   }
 
-  setPrevNext(dishId: string){
-    const index=this.dishIds.indexOf(dishId);
-    this.prev=this.dishIds[(this.dishIds.length + index -1)% this.dishIds.length];
-    this.next=this.dishIds[(this.dishIds.length + index +1)% this.dishIds.length];
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev =
+      this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next =
+      this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
-  goBack(): void{
+  goBack(): void {
     this.location.back();
   }
-
 }
--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -3,7 +3,7 @@ import { Dish } from '../shared/dish';
 // import { DISHES } from '../shared/dishes';
 import { Observable, of } from 'rxjs';
 import { delay, map, catchError } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { baseURL } from '../shared/baseurl';
 import { ProcessHTTPMsgService } from './process-httpmsg.service';
 
@@ -26,7 +26,8 @@ export class DishService {
     // using observable
     // return of(DISHES).pipe(delay(2000));
 
-    return this.http.get<Dish[]>(baseURL + 'dishes')
+    return this.http
+      .get<Dish[]>(baseURL + 'dishes')
       .pipe(catchError(this.processHttpMsgService.handleError));
   }
 
@@ -38,8 +39,9 @@ export class DishService {
 
     // return of(DISHES.filter((dish) => dish.id === id)[0]).pipe(delay(2000));
 
-    return this.http.get<Dish>(baseURL + 'dishes/' + id)
-    .pipe(catchError(this.processHttpMsgService.handleError));
+    return this.http
+      .get<Dish>(baseURL + 'dishes/' + id)
+      .pipe(catchError(this.processHttpMsgService.handleError));
   }
 
   getFeaturedDish(): Observable<Dish> {
@@ -60,7 +62,19 @@ export class DishService {
     // return of(DISHES.map((dish) => dish.id));
 
     return this.getDishes()
-    .pipe(map((dishes) => dishes.map((dish) => dish.id)))
-    .pipe(catchError(error => error));
+      .pipe(map((dishes) => dishes.map((dish) => dish.id)))
+      .pipe(catchError((error) => error));
+  }
+
+  putDish(dish: Dish): Observable<Dish> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+
+    return this.http
+      .put<Dish>(baseURL + 'dishes/' + dish.id, dish, httpOptions)
+      .pipe(catchError(this.processHttpMsgService.handleError));
   }
 }
