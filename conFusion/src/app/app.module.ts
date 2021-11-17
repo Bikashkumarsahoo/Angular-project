7a35090616e4034b98972cbcd669a045b1f798bf
Bikash Kumar Sahoo <biki.sahoo123@gmail.com>
2021-11-17 14:44:33 +0530

    w4.3. directive

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -40,6 +40,7 @@ import { LeaderService } from './services/leader.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -50,7 +51,8 @@ import { baseURL } from './shared/baseurl';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
